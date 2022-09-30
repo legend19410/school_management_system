5a3778564cb60bcfb2d84f6bd7bead0fbf0643c7
legend19410 <francismilton19410@gmail.com>
2022-09-30 09:21:47 -0500

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Greet from './components/Greet';
+import PersonList from './components/PersonList';
+import Button from './components/Button';
+import Input from './components/Input';
+
+const persons = [
+  {
+    firstName: "Milton",
+    lastName: "Francis",
+    age: 26
+  },
+  {
+    firstName: "Shavane",
+    lastName: "Brooks",
+    age: 22
+  }
+]
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Greet name="milton"/>
+      <PersonList names={persons}/>
+      <Button handleClick={(event, id)=> console.log("button is clicked", event, id)}></Button>
+      <Input value="" handleChange={event=>console.log(event)}/>
     </div>
   );
 }
---
+++ src/components/Input.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type InputProps = {
+    value: string,
+    handleChange: (event: React.ChangeEvent<HTMLInputElement>)=> void
+}
+function Input(props: InputProps) {
+  return (
+    <input type="text" value={props.value} onChange={props.handleChange}/>
+  )
+}
+
+export default Input
